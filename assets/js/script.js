868bcc5baa1c97c1d98c384f47e0df75b57d26c7
Ffion Erin <ffionb@btinternet.com>
2023-01-25 20:40:50 +0000

    input not saves and retrieves from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // display date at the top of the planner in the same format as the gif example
 var dateDisplayEl = $('#currentDay');
-
 function displayTime() {
     var dateToday = moment().format('dddd MMMM Do');
     dateDisplayEl.text(dateToday);
@@ -8,12 +7,16 @@ function displayTime() {
 
 displayTime()
 
+
+
 var currentTime = new Date();
 
+// iterate through all elements with the class "textarea" to find the corresponding "hour"
 $('.textarea').each(function() {
   var hour = $(this).closest('tr').find('.hour').text();
   var hourNum = parseInt(hour.replace(/[^0-9]/g, ''));
 
+// add the css classes of past, present or future dependant on the current time of day
   if (hourNum < currentTime.getHours()) {
     $(this).addClass('past');
   } else if (hourNum === currentTime.getHours()) {
@@ -23,43 +26,18 @@ $('.textarea').each(function() {
   }
 });
 
-function saveInput(){
-    let userInput = document.querySelector('#tasks').value;
-    localStorage.setItem('userInput', userInput);
-  }
-  
-  // Code to retrieve the user's input
-  function getInput(){
-    let storedInput = localStorage.getItem('userInput');
-    return storedInput;
-  }
 
+// save the user's input into local storage
+$('.saveBtn').on('click', function() {
+    var textareaVal = $(this).closest('tr').find('.textarea').val();
+    var hour = $(this).closest('tr').find('.hour').text();
+    localStorage.setItem(hour, textareaVal);
+  }); 
   
-$('#saveBtn').click(function(){
-    saveInput();
+  // retrieve the data from local storage and set it to the corresponding textarea
+  $('.textarea').each(function() {
+    var hour = $(this).closest('tr').find('.hour').text();
+    $(this).val(localStorage.getItem(hour));
   });
 
-  
 
-  window.onbeforeunload = function() {
-    saveInput();
-};
-
-getInput()
-// need to add these classes dynamically, depending on what time it is - parseInt, present and future
- 
-// * Present timeblocks for standard business hours when the user scrolls down.
- 
-// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
-
-// * Save the event in local storage when the save button is clicked in that timeblock.
-
-// * Persist events between refreshes of a page
-
-// <!-- 
-//   To enable the user to input and save their tasks to local storage, you can use JavaScript and jQuery. For example, you
-//   can use the jQuery .on() method to attach an event handler to the save button, and use the localStorage.setItem()
-//   method to save the inputted tasks to local storage. For example:
-
-//   $('#save').on('click', function() { -->
