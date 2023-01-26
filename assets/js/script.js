bac5211a5eb66fd5fbc3e0e3a4db0eb9158ecc06
Ffion Erin <ffionb@btinternet.com>
2023-01-26 12:55:07 -0800

    formatted code and added comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,39 +7,36 @@ function displayTime() {
 
 displayTime()
 
-
-
 var currentTime = new Date();
 
 // iterate through all elements with the class "textarea" to find the corresponding "hour"
-$('.textarea').each(function() {
+$('.textarea').each(function () {
     var hour = $(this).closest('tr').find('.hour').text();
+    // split the "hour" so that is only takes the number and leaves the ":00"
     var hourSplit = hour.split(':');
     var hourNum = parseInt(hourSplit[0]);
-  
-  // add the css classes of past, present or future dependant on the current time of day
+
+    // add the css classes of past, present or future dependant on the current time of day
     if (hourNum < currentTime.getHours()) {
-      $(this).addClass('past');
+        $(this).addClass('past');
     } else if (hourNum === currentTime.getHours()) {
-      $(this).addClass('present');
+        $(this).addClass('present');
     } else {
-      $(this).addClass('future');
+        $(this).addClass('future');
     }
-  });
-
-
+});
 
 // save the user's input into local storage
-$('.saveBtn').on('click', function() {
+$('.saveBtn').on('click', function () {
     var textareaVal = $(this).closest('tr').find('.textarea').val();
     var hour = $(this).closest('tr').find('.hour').text();
     localStorage.setItem(hour, textareaVal);
-  }); 
-  
-  // retrieve the data from local storage and set it to the corresponding textarea
-  $('.textarea').each(function() {
+});
+
+// retrieves the data from local storage and set it to the corresponding textarea
+$('.textarea').each(function () {
     var hour = $(this).closest('tr').find('.hour').text();
     $(this).val(localStorage.getItem(hour));
-  });
+});
 
 
