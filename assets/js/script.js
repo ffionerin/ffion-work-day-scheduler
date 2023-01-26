21f5b3fe304e0af62db5502bdc356d1137dfb48f
Ffion Erin <ffionb@btinternet.com>
2023-01-26 10:13:11 -0800

    fixed issue with dynamic styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,20 @@ var currentTime = new Date();
 
 // iterate through all elements with the class "textarea" to find the corresponding "hour"
 $('.textarea').each(function() {
-  var hour = $(this).closest('tr').find('.hour').text();
-  var hourNum = parseInt(hour.replace(/[^0-9]/g, ''));
+    var hour = $(this).closest('tr').find('.hour').text();
+    var hourSplit = hour.split(':');
+    var hourNum = parseInt(hourSplit[0]);
+  
+  // add the css classes of past, present or future dependant on the current time of day
+    if (hourNum < currentTime.getHours()) {
+      $(this).addClass('past');
+    } else if (hourNum === currentTime.getHours()) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
 
-// add the css classes of past, present or future dependant on the current time of day
-  if (hourNum < currentTime.getHours()) {
-    $(this).addClass('past');
-  } else if (hourNum === currentTime.getHours()) {
-    $(this).addClass('present');
-  } else {
-    $(this).addClass('future');
-  }
-});
 
 
 // save the user's input into local storage
